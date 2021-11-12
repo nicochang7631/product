eec0eb9ab217992a6297db10fbb568862fce55e7
Nico Chang <fatrg002@gmail.com>
2021-11-12 16:51:09 +0800

    update function

--- product.py
+++ product.py
@@ -1,36 +1,48 @@
 import os 
-products=[]
 
-if os.path.isfile('products.csv'):#查看csv是否存在
-    print('檔案已存在')
-    #讀取檔案
-    with open('products.csv','r',encoding='utf-8') as f:
+def read_file(filename):
+    products=[]
+    with open(filename,'r',encoding='utf-8') as f:
         for line in f:
             if '商品,價格' in line :
                 continue #繼續
             name,price =  line.strip().split(',')#先把尾巴換行\n去掉,再用split分割
             products.append([name,price])
     print(products)
-else:
-    print('檔案不存在')
+    return products
 
-#讓使用者輸入商品名稱及價格
-while True:
-    name = input("請輸入商品名稱：　")
-    if name == 'q':
-        break
-    price = int(input("請輸入商品價格：　"))
-    
-    products.append([name,price])
+def user_input(products):
+    while True:
+        name = input("請輸入商品名稱：　")
+        if name == 'q':
+            break
+        price = int(input("請輸入商品價格：　"))
+        products.append([name,price])
+    print(products)
+    return products
 
-print(products)
-
-#印出所有購買紀錄
-for p in products:
-    print(p[0],'的價格是',p[1])
-#寫入csv檔
-with open('products.csv','w',encoding='utf-8') as f:
-    f.write('商品,價格\n')
+def print_products(products):
     for p in products:
-        f.write(p[0]+','+str(p[1])+'\n')
+        print(p[0],'的價格是',p[1])
 
+def write_file(filename,products):
+    with open(filename,'w',encoding='utf-8') as f:
+        f.write('商品,價格\n')
+        for p in products:
+            f.write(p[0]+','+str(p[1])+'\n')
+
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename):
+        products = read_file(filename)
+        print('檔案已存在')
+    else:
+        print('檔案不存在')
+    
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv',products)
+
+main()
+    
+  
